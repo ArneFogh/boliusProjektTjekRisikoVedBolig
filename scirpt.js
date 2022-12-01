3b4413c6eca962a8a0c8b715b11abb226dd8f25b
arnefogh <andreasfogh7@gmail.com>
2022-12-01 11:51:42 +0100

    Pushed DAWA API. Did css

--- scirpt.js
+++ scirpt.js
@@ -1,5 +1,5 @@
 function locationsOnMap(){
-    const map = L.map("map").setView([55.442797, 11.795722], 14.5);
+    const map = L.map("map").setView([55.443332, 11.794261], 14.5);
 
     const tiles = L.tileLayer("https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=89TnfA8v1mRlkatNruN7", {
         maxZoom: 19,
@@ -7,16 +7,19 @@ function locationsOnMap(){
             '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
+    L.marker([55.443332, 11.794261]).addTo(map)
+    /*
+    L.icon([55.439519, 11.793025]).addTo(map).bindPopup(`<h2>Netto</h2>
+        <hr>
+            <p>Adresse: Næstvedvej 15, 4100 Ringsted</p>`);
 
-
-    L.marker([55.442797, 11.795722]).addTo(map)
+     */
 
 }
 
 dawaAutocomplete.dawaAutocomplete(document.querySelector('#dawa-autocomplete-input'), {
     select: function(selected) {
         console.log('Valgt adresse: ' + selected.tekst);
-
         locationsOnMap()
     }
 });
