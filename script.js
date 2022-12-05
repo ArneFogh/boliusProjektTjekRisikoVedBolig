6ad2beee6b60e0e34f44b9f4570e78c0de9e228d
arnefogh <andreasfogh7@gmail.com>
2022-12-05 11:09:24 +0100

    Updated the map <3

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 dawaAutocomplete.dawaAutocomplete(document.querySelector('#dawa-autocomplete-input'), {
     select: function(selected) {
+
         console.log('Valgt adresse: ' + selected.tekst +" "+ selected.data.x +" "+ selected.data.y);
         locationsOnMap(selected);
     }
 });
 
-
+const map = L.map("map")
+const tiles = L.tileLayer("https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=89TnfA8v1mRlkatNruN7", {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+map.setView([55.87, 10.64], 6.5);
 function locationsOnMap(selected){
-    const map = L.map("map").setView([selected.data.y, selected.data.x], 14.5);
+    map.setView([selected.data.y, selected.data.x], 14.5);
 
-    const tiles = L.tileLayer("https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=89TnfA8v1mRlkatNruN7", {
-        maxZoom: 19,
-        attribution:
-            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+
+
 
     L.marker([selected.data.y, selected.data.x]).addTo(map).bindPopup('Adresse:' +" "+ selected.tekst)
-    /*
-    L.icon([55.439519, 11.793025]).addTo(map).bindPopup(`<h2>Netto</h2>
-        <hr>
-            <p>Adresse: Næstvedvej 15, 4100 Ringsted</p>`);
 
-     */
+    for (let i = 0; i < stations.length; i++) {
+        L.circle([stations[i].Latitude, stations[i].Longitude]).addTo(map);
+    }
+
 
 }
