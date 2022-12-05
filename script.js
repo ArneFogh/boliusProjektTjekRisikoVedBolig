d9f6a562553842f0aff1ad5626f0b7c599b581e2
arnefogh <andreasfogh7@gmail.com>
2022-12-05 08:56:12 +0100

    Updated dawa autocomplete and map

---
+++ script.js
@@ -0,0 +1,29 @@
+dawaAutocomplete.dawaAutocomplete(document.querySelector('#dawa-autocomplete-input'), {
+    select: function(selected) {
+        console.log('Valgt adresse: ' + selected.tekst +" "+ selected.data.x +" "+ selected.data.y);
+
+        const lat = selected.data.x;
+        const long = selected.data.y;
+        locationsOnMap(selected);
+    }
+});
+
+
+function locationsOnMap(selected){
+    const map = L.map("map").setView([selected.data.y, selected.data.x], 14.5);
+
+    const tiles = L.tileLayer("https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=89TnfA8v1mRlkatNruN7", {
+        maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    L.marker([selected.data.y, selected.data.x]).addTo(map)
+    /*
+    L.icon([55.439519, 11.793025]).addTo(map).bindPopup(`<h2>Netto</h2>
+        <hr>
+            <p>Adresse: Næstvedvej 15, 4100 Ringsted</p>`);
+
+     */
+
+}
